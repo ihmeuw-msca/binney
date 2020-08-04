91198f0a77aa24a6675774383b000a7407e9efb9
Marlena Bannick <mnorwood@uw.edu>
2020-08-03 21:47:47 -0700

    bootstrap tests

--- tests/run/test_bootstrap.py
+++ tests/run/test_bootstrap.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pytest
 from flipper.model.model import LRBinomModel
-from flipper.run.run import BinomBootstrap
+from flipper.run.run import BinomRun
+from flipper.run.bootstrap import BinomBootstrap
 
 from anml.solvers.interface import Solver
 
@@ -26,3 +27,36 @@ def test_binom_bootstrap(df, seed):
         df['total']
     )
     assert not (sample['success'].values == df['success'].values).all()
+
+
+def test_bootstrap_run(df, n):
+    np.random.seed(99)
+    b_run = BinomRun(
+        col_success='success',
+        col_total='total',
+        covariates=['x1'],
+        df=df,
+        solver_method='ipopt'
+    )
+    b_run.fit()
+    b_run.make_uncertainty(n_boots=20)
+    assert b_run.bootstrap.parameters.shape == (20, 2)
+    np.testing.assert_array_almost_equal(
+        b_run.bootstrap.parameters[0, :],
+        np.array([0.99940296, 1.99394325])
+    )
+    np.testing.assert_array_almost_equal(
+        b_run.bootstrap.parameters[5, :],
+        np.array([1.01366631, 1.99569274])
+    )
+    np.testing.assert_array_almost_equal(
+        b_run.bootstrap.parameters.mean(axis=0),
+        b_run.params_opt,
+        decimal=2
+    )
+    uis = np.quantile(b_run.bootstrap.parameters, q=[0.025, 0.975], axis=0)
+    assert all(b_run.params_opt > uis[0, :])
+    assert all(b_run.params_opt < uis[1, :])
+    draws = b_run.predict_draws()
+    assert draws.shape == (20, n)
+    assert all(draws.var(axis=1) > 0)
