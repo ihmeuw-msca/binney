bd3a88f2a3414c9c7c66393acf374e4a2f88bb50
Marlena Bannick <mnorwood@uw.edu>
2020-08-05 13:37:36 -0700

    Documentation for flipper run.

--- src/flipper/run/run.py
+++ src/flipper/run/run.py
@@ -23,6 +23,89 @@ class FlipperRun:
                  splines: Optional[Dict[str, Dict[str, Any]]] = None,
                  solver_method: str = 'scipy', solver_options: Optional[Dict[str, Any]] = None,
                  data_type: str = 'bernoulli'):
+        """
+        Create a model run with flipper. The model can handle either binomial data or Bernoulli data.
+        If you have binomial data, your data will look something like "k successes out
+        of n trials" -- flipper needs to know both k and n. If you have Bernoulli data,
+        your data will look like "individual-record" or "unit-record" data with 1's
+        and 0's. The data needs to be in the same form as the bernoulli type, however all
+        of the "n trials" will be 1, and then the outcome in "success" is either 1 or 0.
+        See the Jupyter notebooks in this repository for an example of Binomial data.
+        The model looks like this in either case:
+
+        .. math::
+
+            k_i \sim Binomial(n_i, p_i) \\
+
+        but where :math:`n_i = 1` if you have Bernoulli data.
+        The goal is to estimate :math:`p` where :math:`p` is the logit of some linear predictor,
+        which may also contain include splines for different covariates. The linear
+        predictor will automatically include an intercept, so do not specify one
+        in your covariates.
+
+        This run class will create uncertainty with the bootstrap method. The particular
+        type of bootstrap re-sampling will depend on whether you have binomial or Bernoulli
+        type data. It is not enforced strictly, but **do not mix the two types of data**,
+        as it will give inaccurate uncertainty quantification.
+
+        Parameters
+        ----------
+        df
+            A pandas data frame with all of the columns in covariates, splines,
+            and col_success and col_total.
+        col_success
+            The column name of the number of successes (:math:`k`).
+        col_total
+            The column name of the number of trials (:math:`n`).
+        covariates
+            A list of column names for covariates to use.
+        splines
+            A dictionary of spline covariates, each of which is a dictionary
+            of spline specifications. For example,
+
+            .. code:: bash
+
+                splines = {
+                    'x1': {
+                        'knots_type': 'domain',
+                        'knots_num': 3,
+                        'degree': 3,
+                        'convex': True
+                    }
+                }
+
+            The list of available options for splines is:
+
+            * :code:`knots_type (str)`: type of knots, one of "domain" or "frequency"
+            * :code:`knots_num (int)`: number of knots
+            * :code:`degree (int)`: degree of the spline
+            * :code:`r_linear (bool)`: include linear tails on the right
+            * :code:`l_linear (bool)`: include linear tails on the left
+            * :code:`increasing (bool)`: impose monotonic increasing constraint on spline shape
+            * :code:`decreasing (bool)`: impose monotonic decreasing constraint on spline shape
+            * :code:`concave (bool)`: impose concavity constraint on spline shape
+            * :code:`convex (bool)`: impose convexity constraint on spline shape
+
+        solver_method
+            Type of solver to use, one of "ipopt" (interior point optimizer -- use this if
+            you have spline shape constraints), or "scipy".
+        solver_options
+            A dictionary of options to pass to your desired solver.
+        data_type
+            The data type: one of "bernoulli" or "binomial"
+
+        Attributes
+        ----------
+        self.params_init
+            Initial parameters for the optimization
+        self.params_opt
+            Optimal parameters found through the fitting process
+        self.bootstrap
+            Bootstrap class that creates uncertainty. After running the
+            :code:`FlipperRun.make_uncertainty()` method you can access
+            the parameter estimates across bootstrap replicates in
+            :code:`self.bootstrap.parameters`.
+        """
 
         # Check the data type
         if data_type not in ['bernoulli', 'binomial']:
@@ -78,11 +161,28 @@ class FlipperRun:
             x_init=self.params_init, options=self.options, data=data
         )
 
-    def fit(self):
+    def fit(self) -> None:
+        """
+        Fit the flipper model after initialization.
+        Optimal parameters are stored in FlipperRun.params_opt.
+        """
         self._fit(solver=self.solver, data=self.lr_specs.data)
         self.params_opt = copy(self.solver.x_opt)
 
     def predict(self, new_df: Optional[pd.DataFrame] = None) -> np.ndarray:
+        """
+        Make predictions based on optimal parameter values.
+
+        Parameters
+        ----------
+        new_df
+            A pandas data frame to make predictions for. Must have all of the covariates
+            used in the fitting.
+
+        Returns
+        -------
+        A numpy array of predictions for the data frame.
+        """
         if new_df is None:
             return self.model.forward(self.params_opt)
         else:
@@ -90,6 +190,19 @@ class FlipperRun:
             return self.model.forward(self.params_opt, mat=self.lr_specs.parameter_set.design_matrix_fe)
 
     def predict_draws(self, df: pd.DataFrame) -> np.ndarray:
+        """
+        Make draws based on the bootstrap parameters.
+
+        Parameters
+        ----------
+        df
+            A pandas data frame to make predictions for. Must have all of the covariates
+            used in the fitting.
+
+        Returns
+        -------
+        A stacked numpy array of draws for each row in the :code:`df`.
+        """
         draw_matrix = []
         for i in range(self.bootstrap.parameters.shape[0]):
             self.bootstrap.lr_specs.configure_new_data(df=df)
@@ -99,18 +212,18 @@ class FlipperRun:
         draw_matrix = np.vstack(draw_matrix)
         return draw_matrix
 
-    def make_uncertainty(self, n_boots: int = 100, **kwargs):
+    def make_uncertainty(self, n_boots: int = 100):
         """
         Runs bootstrap re-sampling to get uncertainty
-        in the parameters.
+        in the parameters. Access parameters in
+        :code:`self.bootstrap.parameters`.
 
         Parameters
         ----------
         n_boots
-        kwargs
+            Number of bootstrap replicates
         """
         self.bootstrap.run_bootstraps(
             n_bootstraps=n_boots,
-            fit_callable=self._fit,
-            **kwargs
+            fit_callable=self._fit
         )
