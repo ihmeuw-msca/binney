ebf5b765dcc49c88b0a3b35c416ac34a1851a220
Marlena Bannick <mnorwood@uw.edu>
2020-08-10 20:54:45 -0700

    Upping version.

--- src/binney/__about__.py
+++ src/binney/__about__.py
@@ -7,7 +7,7 @@ __title__ = "binney"
 __summary__ = "binney: logistic regression tool-kit"
 __uri__ = "https://github.com/ihmeuw-msca/flipper"
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 __author__ = "IHME Math Sciences"
 __email__ = "ihme.math.sciences@gmail.com"
