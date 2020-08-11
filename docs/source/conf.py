05ef8d926b4fdc026cd4d8789166ea9c1253f083
Marlena Bannick <mnorwood@uw.edu>
2020-08-10 21:15:12 -0700

    Change to config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-add_module_names = True
+add_module_names = False
 
 
 def setup(app):
